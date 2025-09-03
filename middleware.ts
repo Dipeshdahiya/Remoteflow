c06daa7f7f5948bc3dbdc61faf213c60c197204d
Dipesh <132298893+Dipeshdahiya@users.noreply.github.com>
2025-09-03 23:08:37 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,48 +1,6 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
 
-// create middleware function
-
-
-import jwt from 'jsonwebtoken';
-import { cookies } from 'next/headers';
-import { NextRequest, NextResponse } from 'next/server';
-
-const auth = async () => {
-  const cookie = cookies()
-  const token = cookie.get('accessToken') || cookie.get('next-auth.session-token') as  string | undefined
-  if (!token) {
-    return {error: 'No token, authorization denied' }
-  }
-
-  return {user: token}
-}
-
-const protectedRoutes = [
-  '/workspace',
-  '/contact-sales',
-  '/communnity',
-  '/api'
-]
-
-
-export default async function middleware(req: NextRequest) {
-  const { user, error } = await auth()
-  const isLoggedIn = !!user
-
-  console.log("Route:- ",req.nextUrl.pathname)
-  console.log("isLoggedIn:- ", isLoggedIn)
-
-  const isProtectedRoute = protectedRoutes.includes(req.nextUrl.pathname)
-
-  if(req.nextUrl.pathname === '/sign-in') {
-    if (isLoggedIn) {
-      return NextResponse.redirect(new URL('/workspace', req.nextUrl))
-    }
-    return NextResponse.next()
-  }
-
-  if (isProtectedRoute && !isLoggedIn) {
-    return NextResponse.redirect(new URL('/sign-in', req.nextUrl))
-  }
-
-  return NextResponse.next();
+export function middleware(req: NextRequest) {
+  return NextResponse.next()
 }
